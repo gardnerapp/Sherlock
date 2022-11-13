a7cda74814572ce0d0a701f731fff7277f0dfc58
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2022-11-13 11:57:33 -0500

    Fix rspec test for Request object

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,7 +1,8 @@
-# TODO Add rspec-file_fixtures
+# frozen_string_literal: true
+
 require "sherlock"
 describe Sherlock::Request do
-  @log = {
+  log = {
      "remote_addr": "185.7.214.117",
      "remote_user": "-",
      "time_local": "16/Aug/2022:08:46:06 +0000",
@@ -12,10 +13,10 @@ describe Sherlock::Request do
      "http_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
    }
 
-   %w[remote_addr time_local request status http_user_agent].each do |attr|
+   %i[remote_addr time_local request status http_user_agent].each do |attr|
      it "Request has #{attr}" do
-       req = Sherlock::Request.new(@log)
-       expect(eval("req.#{attr}")). to eql(@log[attr])
+       req = Sherlock::Request.new(log)
+       expect(eval("req.#{attr}")).to eql(log[attr])
      end
    end
 end
